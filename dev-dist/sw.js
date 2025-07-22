5250da5d189bdf16ca9618a30ff0c598604cf920
GreenTea0413 <kimbosung22@naver.com>
2025-07-22 20:03:25 +0900

    [Fix] 모바일 확대 기능 삭제

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.gfj6euh52po"
+    "revision": "0.b4i4fa94fgg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
