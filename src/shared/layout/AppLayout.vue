02926161c93c2cbb148403e7a310fd1ce5a6b7d3
GreenTea0413 <kimbosung22@naver.com>
2025-07-22 16:12:49 +0900

    [Fix] pwa 세로 스크롤 수정 && 업데이트 팝업 추가

--- src/shared/layout/AppLayout.vue
+++ src/shared/layout/AppLayout.vue
@@ -1,24 +1,16 @@
 <template>
-  <div class="layout-wrapper">
+  <div class="flex justify-center bg-white min-h-screen">
+    <PWAUpdatePrompt />
     <Header />
-    <div class="layout-container">
+    <div class="w-full max-w-[360px] md:max-w-[768px] bg-gray-BGDim px-4 sm:px-5 pt-16 pb-12">
       <slot />
     </div>
-    <Navigator/>
+    <Navigator />
   </div>
 </template>
 
 <script setup lang="ts">
 import Header from '@/shared/ui/atoms/Header.vue'
-import Navigator from '@/shared/ui/atoms/Navigator.vue';
+import Navigator from '@/shared/ui/atoms/Navigator.vue'
+import PWAUpdatePrompt from '@/shared/ui/organisms/PWAUpdatePrompt.vue'
 </script>
-
-<style scoped>
-.layout-wrapper {
-  @apply flex justify-center bg-white min-h-screen;
-}
-
-.layout-container {
-  @apply w-full max-w-[360px] md:max-w-[768px] bg-gray-BGDim px-4 sm:px-5 pt-16 pb-12;
-}
-</style>
