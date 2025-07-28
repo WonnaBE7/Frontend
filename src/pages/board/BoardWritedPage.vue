3701b34d6974a9bd00881c6bc9deb3d52475e83d
GreenTea0413 <kimbosung22@naver.com>
2025-07-29 01:18:38 +0900

    [fix] #32 fix build error

--- src/pages/board/BoardWritedPage.vue
+++ src/pages/board/BoardWritedPage.vue
@@ -6,5 +6,5 @@
    
    <script setup lang="ts">
    import AppLayout from '@/shared/layout/AppLayout.vue';
-   import WritedBox from '@/features/board/board-writed/ui/WriteBox.vue';
+   import WritedBox from '@/features/board/board-writed/ui/WritedBox.vue';
    </script>
